68d54afe75a9d2b952d38533805187af6d270cee
attila.szombati <attila.szombati@lensa.com>
2021-08-07 12:47:49 +0200

    linter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# pylint:disable=missing-function-docstring, missing-module-docstring
 from orm.models import TwitterDataModel
 from scraper.context import get_mysql_db, get_secrets, init_database
 from scraper.twitter import apply_all_fixture
