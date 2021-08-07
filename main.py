6005d95fa02608fae46b23e6bd3c8481be5712c7
attila.szombati <attila.szombati@lensa.com>
2021-08-07 16:18:18 +0200

    create barack obama scraper

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 # pylint:disable=missing-function-docstring, missing-module-docstring
 import json
 
-from orm.models import TwitterDataModelElonMusk, TwitterDataModelJeffBezos
+from orm.models import (
+    TwitterDataModelElonMusk,
+    TwitterDataModelJeffBezos,
+    TwitterDataModelBarackObama
+)
 from scraper.context import get_mysql_db, get_secrets, init_database
 from scraper.twitter import apply_all_fixture
 
 tables = {
     'elonmusk': TwitterDataModelElonMusk,
-    'JeffBezos': TwitterDataModelJeffBezos
+    'JeffBezos': TwitterDataModelJeffBezos,
+    'BarackObama': TwitterDataModelBarackObama,
 }
+
 
 def handler(request):
     request = request.get_data()
