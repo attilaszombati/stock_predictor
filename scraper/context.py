8b929a6c401d2113bf2858320bdea4a1b69b46f9
szombatiattila <attila.szombati@aliz.ai>
2022-05-30 23:14:05 +0200

    remove echo mode from engine

--- scraper/context.py
+++ scraper/context.py
@@ -5,7 +5,7 @@ from sqlalchemy_utils import database_exists, create_database
 
 
 def connect_database_sqlalchemy():
-    engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
+    engine = sqlalchemy.create_engine('sqlite:///:memory:')
     if not database_exists(engine.url):
         create_database(engine.url)
 
