19af5badc5d24a9e45b28cc8c854ec3cb3d285a9
szombatiattila <attila.szombati@aliz.ai>
2022-06-06 12:39:23 +0200

    try

--- cloud_function/setup.py
+++ cloud_function/setup.py
@@ -16,4 +16,22 @@ setuptools.setup(
     zip_safe=True,
     packages=find_packages(),
     python_requires=">3.7.0, <3.11.0",
+    install_requires=[
+        "peewee==3.14.4",
+        "pandas==1.4.1",
+        "snscrape~=0.4.3.20220106",
+        "pytest==6.2.4",
+        "praw==7.4.0",
+        "lint==1.2.1",
+        "fastparquet==0.8.1",
+        "SQLAlchemy==1.4.35",
+        "SQLAlchemy-Utils==0.38.2",
+        "pg8000==1.26.1",
+        "google-cloud-secret-manager",
+        "google-cloud-storage==2.3.0",
+        "google-api-python-client==2.45.0",
+        "oauth2client==4.1.3",
+        "pydantic~=1.9.0",
+    ]
+
 )
