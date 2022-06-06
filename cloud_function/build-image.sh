0c2da14f4aa0a46feffa629628696b2c3985ef5a
szombatiattila <attila.szombati@aliz.ai>
2022-06-06 19:10:28 +0200

    try

--- cloud_function/build-image.sh
+++ cloud_function/build-image.sh
@@ -5,5 +5,5 @@ echo ${GCP_PROJECT_KEY} | base64 --decode --ignore-garbage >$HOME/gcloud-service
 echo 'export GOOGLE_CLOUD_KEYS=$(cat $HOME/gcloud-service-key.json)' >>$BASH_ENV
 echo 'export TAG=${CIRCLE_SHA1}' >> $BASH_ENV
 echo 'export LATEST_TAG=latest' >> $BASH_ENV
-echo 'export IMAGE_NAME=$CIRCLE_PROJECT_REPONAME' >>$BASH_ENV && source $BASH_ENV
+echo 'export IMAGE_NAME=twitter-scraper' >>$BASH_ENV && source $BASH_ENV
 docker build -t gcr.io/$GOOGLE_PROJECT_ID/$IMAGE_NAME -t gcr.io/$GOOGLE_PROJECT_ID/$IMAGE_NAME:$TAG ./cloud_function
