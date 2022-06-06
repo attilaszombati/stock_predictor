b1c97e73c3c8d5401d24b2d16b4961d2f8a5f1ec
szombatiattila <attila.szombati@aliz.ai>
2022-06-06 02:43:23 +0200

    terraform fmt

--- main.tf
+++ main.tf
@@ -22,15 +22,15 @@ resource "google_storage_bucket" "cloud_function_bucket" {
 # place the zip-ed code in the bucket
 resource "google_storage_bucket_object" "hello_world_zip" {
   name   = "twitter_scraper"
-  bucket = "${google_storage_bucket.cloud_function_bucket.name}"
+  bucket = google_storage_bucket.cloud_function_bucket.name
   source = "${path.root}/cloud_function.zip"
 }
 
 resource "google_cloudfunctions_function" "twitter_scraper_function" {
   name                  = "twitter-scraper"
   available_memory_mb   = 512
-  source_archive_bucket = "${google_storage_bucket.cloud_function_bucket.name}"
-  source_archive_object = "${google_storage_bucket_object.hello_world_zip.name}"
+  source_archive_bucket = google_storage_bucket.cloud_function_bucket.name
+  source_archive_object = google_storage_bucket_object.hello_world_zip.name
   timeout               = 540
   entry_point           = "handler"
   trigger_http          = true
