6b57a6093aad11d4c6e26d4480ed629c4d6bee2c
szombatiattila <attila.szombati@aliz.ai>
2022-09-25 11:28:27 +0200

    debug print

--- crypto_data_function/main.py
+++ crypto_data_function/main.py
@@ -30,6 +30,9 @@ def handler():
     bars = api.get_crypto_bars("BTCUSD", TimeFrame.Minute).df.iloc[[-1]]
     timestamp = time.time()
     bars.to_parquet(path=f'/tmp/BTCUSD_{timestamp}.pq', compression='snappy')
+    print("X" * 50)
+    print(bars.iloc[[-1]])
+    print("X" * 50)
     storage = CloudStorageUtils()
     storage.save_data_to_cloud_storage(bucket_name='crypto_data_collection',
                                        file_name=f'BTCUSD/{timestamp}.pq',
