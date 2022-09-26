183ae5596781f42a288b30eb9e711960b206eb6d
szombatiattila <attila.szombati@aliz.ai>
2022-09-27 01:33:42 +0200

    fckfckfckfck

--- service-accounts.tf
+++ service-accounts.tf
@@ -3,18 +3,12 @@ resource "google_service_account" "cloudrun-invoker" {
   display_name = "SA for invoke cloud run services"
 }
 
-resource "google_service_account" "crypto-data-scraper-invoker" {
-  account_id   = "crypto-data-scraper-invoker"
-  display_name = "SA for invoke cloud run services"
-}
-
 resource "google_project_iam_binding" "cloud-run-invoker-iam" {
   project = "attila-szombati-sandbox"
   role    = "roles/run.invoker"
 
   members = [
-    "serviceAccount:${google_service_account.cloudrun-invoker.email}",
-    "serviceAccount:${google_service_account.crypto-data-scraper-invoker.email}"
+    "serviceAccount:${google_service_account.cloudrun-invoker.email}"
   ]
 }
 
