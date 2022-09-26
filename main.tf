015ccfea9e2ce92a913fe9e1211a08b2f21a1bf6
szombatiattila <attila.szombati@aliz.ai>
2022-09-27 01:24:28 +0200

    fckfck

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "google_cloud_run_service" "crypto-data-scraper" {
           container_port = 8080
         }
       }
-      timeout_seconds = 540
+      timeout_seconds      = 540
       service_account_name = google_service_account.storage-admin.email
     }
   }
