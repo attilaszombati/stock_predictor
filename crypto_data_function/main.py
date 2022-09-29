1d09b993302f9cd5dfef32bd544acd0209cb3fcc
szombatiattila <attila.szombati@aliz.ai>
2022-09-29 11:20:46 +0200

    try to fetch history

--- crypto_data_function/main.py
+++ crypto_data_function/main.py
@@ -1,9 +1,13 @@
 import logging
 import os
+from datetime import datetime
 
+import pandas as pd
+import yfinance as yf
 from alpaca.data import TimeFrame
 from alpaca_trade_api import REST
 from flask import Flask, request
+from pandas import Timestamp
 
 from utils.cloud_storage import CloudStorageUtils
 
@@ -26,9 +30,36 @@ def main(symbol: str = 'BTCUSD'):
     latest_bar_data = data.index.format()[0].replace(" ", "_")
     data.to_parquet(path=f'/tmp/{latest_bar_data}_{symbol}.pq', compression='snappy')
     logger.warning(f"Saving {latest_bar_data} data for {symbol} to cloud storage")
+
     gcs_storage.save_data_to_cloud_storage(bucket_name='crypto_data_collection',
                                            file_name=f'{symbol}/{latest_bar_data}_{symbol}.pq',
                                            parquet_file=f'/tmp/{latest_bar_data}_{symbol}.pq')
+
+    return latest_bar_data
+
+
+def historical_data(symbol: str = 'BTC-USD'):
+    gcs_storage = CloudStorageUtils()
+
+    freq = '-7d'
+    dates = pd.date_range(start=datetime.now().date().strftime('%Y-%m-%d'), periods=670, freq=freq,
+                          inclusive=None).strftime('%Y-%m-%d').to_list()
+
+    shift_dates = [[i, j] for i, j in zip(dates, dates[1:])]
+
+    for one_week in shift_dates:
+        data = yf.download(symbol, start=one_week[1], end=one_week[0], interval="1m", actions=True)
+        data.rename(columns=str.lower, inplace=True)
+        data.index.rename("timestamp", inplace=True)
+        latest_bar_data = data.index.format()[0].replace(" ", "_")
+
+        data.to_parquet(path=f'/tmp/{symbol}.pq', compression='snappy')
+
+        gcs_storage.save_data_to_cloud_storage(bucket_name='crypto_data_collection',
+                                               file_name=f'{symbol}/{latest_bar_data}_{symbol}.pq',
+                                               parquet_file=f'/tmp/{latest_bar_data}_{symbol}.pq')
+
+        logger.warning(f"Saving historical data for {symbol} to cloud storage for the week {one_week}")
 
 
 @app.route("/", methods=['POST'])
@@ -36,11 +67,13 @@ def handler():
     data = request.get_json()
     logger.warning(f"The data is : {data}")
     symbols = data.get('SYMBOLS')
-    if data.get('SCRAPING_TYPE') == 'history':
-        pass
-    else:
-        for symbol in symbols:
+
+    for symbol in symbols:
+        if data.get('SCRAPING_TYPE') == 'history':
+            historical_data(symbol=symbol)
+        else:
             main(symbol=symbol)
+
     return "OK"
 
 
