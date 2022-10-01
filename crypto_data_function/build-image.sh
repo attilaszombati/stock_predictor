d6578be3b567969f1ee9aaa990e28a39b06a52de
szombatiattila <attila.szombati@aliz.ai>
2022-10-01 12:56:38 +0200

    remove push

--- crypto_data_function/build-image.sh
+++ crypto_data_function/build-image.sh
@@ -8,5 +8,3 @@ echo 'export LATEST_TAG=latest' >>$BASH_ENV
 echo 'export IMAGE_NAME=crypto-data-scraper' >>$BASH_ENV && /bin/bash -c "source ${BASH_ENV}"
 # docker pull gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest || true
 docker build --cache-from gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest -t gcr.io/attila-szombati-sandbox/crypto-data-scraper:$CIRCLE_SHA1 -t gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest ./crypto_data_function
-docker push gcr.io/attila-szombati-sandbox/crypto-data-scraper:$CIRCLE_SHA1
-docker push gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest
