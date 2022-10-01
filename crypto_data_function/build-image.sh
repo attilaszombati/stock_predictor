52dc51acf24b1d7ac3a260fd1e408da62df71e63
szombatiattila <attila.szombati@aliz.ai>
2022-10-01 12:39:59 +0200

    more print

--- crypto_data_function/build-image.sh
+++ crypto_data_function/build-image.sh
@@ -7,5 +7,9 @@ echo 'export TAG=${CIRCLE_SHA1}' >>$BASH_ENV
 echo 'export LATEST_TAG=latest' >>$BASH_ENV
 echo 'export IMAGE_NAME=crypto-data-scraper' >>$BASH_ENV && /bin/bash -c "source ${BASH_ENV}"
 echo $BASH_ENV
+echo $CIRCLECI
+echo $CIRCLE_BRANCH
+echo $CIRCLE_BUILD_NUM
+echo $CIRCLE_SHA1
 # docker pull gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest || true
 docker build --cache-from gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest -t gcr.io/attila-szombati-sandbox/crypto-data-scraper:$IMAGE_TAG -t gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest ./crypto_data_function
