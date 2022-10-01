c1c4f6cebcde3570deae90c2d89a31278133cc55
szombatiattila <attila.szombati@aliz.ai>
2022-10-01 11:46:24 +0200

    try

--- crypto_data_function/build-image.sh
+++ crypto_data_function/build-image.sh
@@ -5,6 +5,6 @@ echo ${GCP_PROJECT_KEY} | base64 --decode --ignore-garbage >$HOME/gcloud-service
 echo 'export GOOGLE_CLOUD_KEYS=$(cat $HOME/gcloud-service-key.json)' >>$BASH_ENV
 echo 'export TAG=${CIRCLE_SHA1}' >>$BASH_ENV
 echo 'export LATEST_TAG=latest' >>$BASH_ENV
-echo 'export IMAGE_NAME=crypto-data-scraper' >>$BASH_ENV && source $BASH_ENV
+echo 'export IMAGE_NAME=crypto-data-scraper' >>$BASH_ENV
 docker pull gcr.io/attila-szombati-sandbox/${IMAGE_NAME}:${LATEST_TAG} || true
 docker build --cache-from gcr.io/attila-szombati-sandbox/${IMAGE_NAME}:${LATEST_TAG} -t gcr.io/attila-szombati-sandbox/${IMAGE_NAME}:${TAG} -t gcr.io/attila-szombati-sandbox/${IMAGE_NAME}:${LATEST_TAG} .
