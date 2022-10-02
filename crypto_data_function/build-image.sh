0800bf243b3b3fb4b991532ca10c0585affe8c54
szombatiattila <attila.szombati@aliz.ai>
2022-10-02 16:15:08 +0200

    try not context

--- crypto_data_function/build-image.sh
+++ crypto_data_function/build-image.sh
@@ -6,5 +6,8 @@ echo 'export GOOGLE_CLOUD_KEYS=$(cat $HOME/gcloud-service-key.json)' >>$BASH_ENV
 echo 'export TAG=${CIRCLE_SHA1}' >>$BASH_ENV
 echo 'export LATEST_TAG=latest' >>$BASH_ENV
 echo 'export IMAGE_NAME=crypto-data-scraper' >>$BASH_ENV && /bin/bash -c "source ${BASH_ENV}"
+curl -sSL https://sdk.cloud.google.com | bash
+/bin/sh export=$PATH:/root/google-cloud-sdk/bin
+gcloud auth configure-docker --quiet --project attila-szombati-sandbox gcr.io/attila-szombati-sandbox/crypto-data-scraper
 docker pull gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest || true
 docker build --cache-from gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest -t gcr.io/attila-szombati-sandbox/crypto-data-scraper:$CIRCLE_SHA1 -t gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest ./crypto_data_function
