26a5a9a09d54c6fe7291b8e36e0ef12479028dd5
szombatiattila <attila.szombati@aliz.ai>
2022-10-02 13:48:54 +0200

    create config for history update

--- crypto_data_function/main.py
+++ crypto_data_function/main.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'GszzkYig0nXUMquNyz0Viw1R95oiSKi0KjJOcz4C'
 
 
 def fingerprint_is_up_to_date(fingerprint: str = None) -> bool:
-    now_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S-00:00")
+    now_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S-00:00')
 
     return now_timestamp < fingerprint
 
@@ -31,10 +31,10 @@ def main(api, symbol: str = 'BTCUSD'):
         file_name=f'{symbol}/fingerprint.csv'
     )
 
-    logger.warning(f"Current fingerprint for {symbol} is {fingerprint}")
+    logger.warning(f'Current fingerprint for {symbol} is {fingerprint}')
 
-    if not fingerprint_is_up_to_date(fingerprint=fingerprint):
-        logger.warning(f"Fingerprint is not up to date for {symbol}")
+    if not fingerprint_is_up_to_date(fingerprint=fingerprint, update_history=True):
+        logger.warning(f'Fingerprint is not up to date for {symbol}')
         historical_data(api=api, symbol=symbol, start_timestamp=fingerprint)
 
     time_frame = TimeFrame.Minute
@@ -43,9 +43,9 @@ def main(api, symbol: str = 'BTCUSD'):
         symbol=symbol,
         timeframe=time_frame.value
     ).df.iloc[[-1]]
-    latest_bar_data = data.index.format()[0].replace(" ", "_")
+    latest_bar_data = data.index.format()[0].replace(' ', '_')
     data.to_parquet(path=f'/tmp/{latest_bar_data}_{symbol}.pq', compression='snappy')
-    logger.warning(f"Saving {latest_bar_data} data for {symbol} to cloud storage")
+    logger.warning(f'Saving {latest_bar_data} data for {symbol} to cloud storage')
 
     gcs_storage.save_data_to_cloud_storage(bucket_name='crypto_data_collection',
                                            file_name=f'{symbol}/{latest_bar_data}_{symbol}.pq',
@@ -53,7 +53,7 @@ def main(api, symbol: str = 'BTCUSD'):
 
     updated_fingerprint = data.index.format()[-1]
 
-    logger.warning(f"Setting fingerprint for {symbol} to {updated_fingerprint}")
+    logger.warning(f'Setting fingerprint for {symbol} to {updated_fingerprint}')
 
     gcs_storage.set_fingerprint_for_user(
         bucket_name='crypto_data_collection',
@@ -64,21 +64,35 @@ def main(api, symbol: str = 'BTCUSD'):
     return updated_fingerprint
 
 
-def historical_data(api, symbol: str = 'BTCUSD', start_timestamp: str = '2009-01-01T00:00:00-00:00'):
+def historical_data(api, symbol: str = 'BTCUSD', start_timestamp: str = '2009-01-01T00:00:00-00:00',
+                    update_history=False):
     gcs_storage = CloudStorageUtils()
-
+    time_format = '%Y-%m-%dT%H:%M:%S-00:00'
     freq = '3m'
 
-    time_format = "%Y-%m-%dT%H:%M:%S-00:00"
-    offset_time = datetime.now() + timedelta(weeks=15)
-    end_timestamp = offset_time.strftime(time_format)
+    if update_history:
+        offset_time = datetime.now() + timedelta(days=1)
+        end_timestamp = offset_time.strftime(time_format)
+        range_config = {
+            'start': start_timestamp,
+            'end': end_timestamp,
+            'inclusive': 'both'
+        }
+    else:
+        offset_time = datetime.now() + timedelta(weeks=15)
+        end_timestamp = offset_time.strftime(time_format)
+        range_config = {
+            'start': start_timestamp,
+            'end': end_timestamp,
+            'freq': freq,
+            'inclusive': 'both'
+        }
 
-    dates = pd.date_range(start=start_timestamp, end=end_timestamp, freq=freq,
-                          inclusive='both').strftime(time_format).to_list()
+    dates = pd.date_range(**range_config).strftime(time_format).to_list()
     shift_dates = [[i, j] for i, j in zip(dates, dates[1:])]
 
     for start, end in shift_dates:
-        logger.warning(f"Saving historical data from : {start} to : {end} for {symbol} to cloud storage")
+        logger.warning(f'Saving historical data from : {start} to : {end} for {symbol} to cloud storage')
         data = api.get_crypto_bars(
             symbol=symbol,
             timeframe=TimeFrame.Minute,
@@ -86,9 +100,9 @@ def historical_data(api, symbol: str = 'BTCUSD', start_timestamp: str = '2009-01
             end=end
         ).df
         if not data.empty:
-            latest_bar_data = data.index.format()[-1].replace(" ", "_")
+            latest_bar_data = data.index.format()[-1].replace(' ', '_')
             data.to_parquet(path=f'/tmp/{latest_bar_data}_{symbol}.pq', compression='snappy')
-            logger.warning(f"Saving {latest_bar_data} data for {symbol} to cloud storage")
+            logger.warning(f'Saving {latest_bar_data} data for {symbol} to cloud storage')
 
             gcs_storage.save_data_to_cloud_storage(bucket_name='crypto_data_collection',
                                                    file_name=f'{symbol}/{latest_bar_data}_{symbol}.pq',
@@ -96,7 +110,7 @@ def historical_data(api, symbol: str = 'BTCUSD', start_timestamp: str = '2009-01
 
     fingerprint = data.index.format()[-1]
 
-    logger.warning(f"Setting fingerprint for {symbol} to {fingerprint}")
+    logger.warning(f'Setting fingerprint for {symbol} to {fingerprint}')
 
     gcs_storage.set_fingerprint_for_user(
         bucket_name='crypto_data_collection',
@@ -105,10 +119,10 @@ def historical_data(api, symbol: str = 'BTCUSD', start_timestamp: str = '2009-01
     )
 
 
-@app.route("/", methods=['POST'])
+@app.route('/', methods=['POST'])
 def handler():
     data = request.get_json()
-    logger.warning(f"The data is : {data}")
+    logger.warning(f'The data is : {data}')
     symbols = data.get('SYMBOLS')
     start_timestamp = data.get('START_DATE')
 
@@ -122,8 +136,8 @@ def handler():
         else:
             main(api=api, symbol=symbol)
 
-    return "OK"
+    return 'OK'
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
