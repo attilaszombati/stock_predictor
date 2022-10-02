35ff6d51a9bdb740a4ba5dff8364516eb517b3c5
szombatiattila <attila.szombati@aliz.ai>
2022-10-02 16:08:51 +0200

    try same context

--- crypto_data_function/build-image.sh
+++ crypto_data_function/build-image.sh
@@ -6,5 +6,5 @@ echo 'export GOOGLE_CLOUD_KEYS=$(cat $HOME/gcloud-service-key.json)' >>$BASH_ENV
 echo 'export TAG=${CIRCLE_SHA1}' >>$BASH_ENV
 echo 'export LATEST_TAG=latest' >>$BASH_ENV
 echo 'export IMAGE_NAME=crypto-data-scraper' >>$BASH_ENV && /bin/bash -c "source ${BASH_ENV}"
-# docker pull gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest || true
+docker pull gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest || true
 docker build --cache-from gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest -t gcr.io/attila-szombati-sandbox/crypto-data-scraper:$CIRCLE_SHA1 -t gcr.io/attila-szombati-sandbox/crypto-data-scraper:latest ./crypto_data_function
