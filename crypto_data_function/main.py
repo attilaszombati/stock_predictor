d84d1c70270e59a159212306e5b07797fa45adb5
szombatiattila <attila.szombati@aliz.ai>
2022-10-03 22:19:03 +0200

    timestamp conversion

--- crypto_data_function/main.py
+++ crypto_data_function/main.py
@@ -29,6 +29,7 @@ def fingerprint_is_up_to_date(fingerprint: str = None) -> bool:
 def convert_columns_to_float64(df, columns):
     for column in columns:
         df[column] = df[column].astype('float64')
+    df["timestamp"] = df["timestamp"].astype(int) // 10 ** 9
     df.reset_index(inplace=True)
 
     return df
