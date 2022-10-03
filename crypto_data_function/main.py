b57bd73da6baacfd8e9427d3ea803c25cf5a8fd4
szombatiattila <attila.szombati@aliz.ai>
2022-10-03 23:17:17 +0200

    try

--- crypto_data_function/main.py
+++ crypto_data_function/main.py
@@ -56,9 +56,9 @@ def main(api, symbol: str = 'BTCUSD'):
         timeframe=time_frame.value
     ).df.iloc[[-1]]
 
+    latest_bar_data = data.index.format()[-1].replace(' ', '_')
     converted_data = convert_columns_to_float64(df=data, columns=['open', 'high', 'low', 'close', 'volume'])
 
-    latest_bar_data = converted_data["timestamp"].format()[0].replace(' ', '_')
     converted_data.to_parquet(path=f'/tmp/{latest_bar_data}_{symbol}.pq', compression='snappy')
     logger.warning(f'Saving {latest_bar_data} data for {symbol} to cloud storage')
 
@@ -66,7 +66,7 @@ def main(api, symbol: str = 'BTCUSD'):
                                            file_name=f'{symbol}/{latest_bar_data}_{symbol}.pq',
                                            parquet_file=f'/tmp/{latest_bar_data}_{symbol}.pq')
 
-    updated_fingerprint = converted_data["timestamp"].format()[-1]
+    updated_fingerprint = data.index.format()[-1]
 
     logger.warning(f'Setting fingerprint for {symbol} to {updated_fingerprint}')
 
@@ -116,8 +116,8 @@ def historical_data(api, symbol: str = 'BTCUSD', start_timestamp: str = '2009-01
         ).df
 
         if not data.empty:
+            latest_bar_data = data.index.format()[-1].replace(' ', '_')
             converted_data = convert_columns_to_float64(df=data, columns=['open', 'high', 'low', 'close', 'volume'])
-            latest_bar_data = converted_data["timestamp"].format()[-1].replace(' ', '_')
             converted_data.to_parquet(path=f'/tmp/{latest_bar_data}_{symbol}.pq', compression='snappy')
             logger.warning(f'Saving {latest_bar_data} data for {symbol} to cloud storage')
 
@@ -125,7 +125,7 @@ def historical_data(api, symbol: str = 'BTCUSD', start_timestamp: str = '2009-01
                                                    file_name=f'{symbol}/{latest_bar_data}_{symbol}.pq',
                                                    parquet_file=f'/tmp/{latest_bar_data}_{symbol}.pq')
 
-    fingerprint = converted_data["timestamp"].format()[-1]
+    fingerprint = data.index.format()[-1]
 
     logger.warning(f'Setting fingerprint for {symbol} to {fingerprint}')
 
