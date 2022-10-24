07f958ef21bd2b497c9b8dc463fd35c1a86239f5
szombatiattila <attila.szombati@aliz.ai>
2022-10-25 01:58:57 +0200

    fix image name

--- predictor_api/build-image.sh
+++ predictor_api/build-image.sh
@@ -5,5 +5,5 @@ echo ${GCP_PROJECT_KEY} | base64 --decode --ignore-garbage >$HOME/gcloud-service
 echo 'export GOOGLE_CLOUD_KEYS=$(cat $HOME/gcloud-service-key.json)' >>$BASH_ENV
 echo 'export TAG=${CIRCLE_SHA1}' >>$BASH_ENV
 echo 'export LATEST_TAG=latest' >>$BASH_ENV
-echo 'export IMAGE_NAME=twitter-scraper' >>$BASH_ENV && source $BASH_ENV
+echo 'export IMAGE_NAME=predictor_api' >>$BASH_ENV && source $BASH_ENV
 docker build --cache-from gcr.io/attila-szombati-sandbox/predictor_api:latest -t gcr.io/attila-szombati-sandbox/predictor_api:$CIRCLE_SHA1 -t gcr.io/attila-szombati-sandbox/predictor_api:latest ./predictor_api
