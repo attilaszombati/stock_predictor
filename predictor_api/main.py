b2ff5b4b1051018b18a385017a8f1af0261cd4ff
szombatiattila <attila.szombati@aliz.ai>
2022-10-25 11:55:42 +0200

    .item(0)

--- predictor_api/main.py
+++ predictor_api/main.py
@@ -29,7 +29,7 @@ def handler():
     twitter_data = data.get('TWITTER_POST_DATA', [])
     data = convert_to_np_array(twitter_data)
     prediction = main(data=data)
-    return {'prediction': prediction}
+    return {'prediction': prediction.item(0)}
 
 
 if __name__ == "__main__":
