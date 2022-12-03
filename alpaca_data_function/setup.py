32568e672066d77fc5c34ab857b231d20764e8af
szombatiattila <attila.szombati@aliz.ai>
2022-12-04 00:59:19 +0100

    linter check for alpaca_data_function

---
+++ alpaca_data_function/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from setuptools import find_packages
+
+test_requirements = [
+    "pytest",
+    "pytest-cov",
+    "black",
+    "mypy",
+]
+
+setuptools.setup(
+    name="alpaca-data-function",
+    version="0.1.0",
+    description="alpaca data function",
+    maintainer="Attila Szombati",
+    zip_safe=True,
+    packages=find_packages(),
+    python_requires=">3.7.0, <3.11.0",
+    install_requires=[
+        "pandas==1.4.1",
+        "snscrape~=0.4.3.20220106",
+        "pytest==6.2.4",
+        "praw==7.4.0",
+        "lint==1.2.1",
+        "fastparquet==0.8.1",
+        "SQLAlchemy==1.4.35",
+        "SQLAlchemy-Utils==0.38.2",
+        "pg8000==1.26.1",
+        "google-cloud-secret-manager",
+        "google-cloud-storage==2.3.0",
+        "google-api-python-client==2.45.0",
+        "oauth2client==4.1.3",
+        "pydantic~=1.9.0",
+    ],
+)
